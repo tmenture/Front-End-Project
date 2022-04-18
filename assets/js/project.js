870e2c8b940cac7eb7e119ebab0a10e01611ddc4
Agatha Harkness <caitlinmarie.clifford@gmail.com>
2022-04-18 19:40:40 -0400

    updated styling

--- assets/js/project.js
+++ assets/js/project.js
@@ -12,63 +12,39 @@ function getLocation () {
 };
 
 $(document).on("click", "#btn-t", function (event) {
-
     event.preventDefault();
     getLocation()
     //callApi();
 });
 
 var callApi = function (userZipCode) {
-
-             fetch('https://app.ticketmaster.com/discovery/v2/events.json?postalCode=' + userZipCode + "&radius=50&unit=miles&apikey=u0qeyJGVcW318hNAeQdpuAQrDfoV5v5R")
-
-                .then((response) => {
-
-                    if (response.ok) {
-
-                        return response.json();
-
-                    } else {
-
-                        throw new Error("Network Response Error");
-
-                    }
-                })
-
-                .then(data => {
-
-                console.log(data);
-
-                displayData(data);
-
-            })
-
-            .catch((error) => console.error("Fetch Error:", error));
-
+    fetch('https://app.ticketmaster.com/discovery/v2/events.json?postalCode=' + userZipCode + "&radius=50&unit=miles&apikey=u0qeyJGVcW318hNAeQdpuAQrDfoV5v5R")
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error("Network Response Error");
+            }
+        })
+        .then(data => {
+            console.log(data);
+            displayData(data);
+        })
+        .catch((error) => console.error("Fetch Error:", error));
 };
 
 var displayData = function (data) {
-
     for (var i = 0; i < data._embedded.events.length; i++) {
-
         const eventEl = data._embedded.events[i];
-
         const eventElDiv = document.getElementById("fetch-container");
-
         const event = eventEl.name;
-
         const header = document.createElement("h1");
-
         header.innerHTML = event;
-
         eventElDiv.appendChild(header);
-
     };
-
 };
 
 $(document).on("click", "#btn-t", function (event) {
-
     getLocation();
    // getWeather();
    // getMovie();
@@ -192,4 +168,4 @@ function displayRecipe(data) {
     link.textContent = data.meals[0].strSource;
     link.setAttribute("href", data.meals[0].strSource);
     recipeHolder.appendChild(link);
-}
+}}
